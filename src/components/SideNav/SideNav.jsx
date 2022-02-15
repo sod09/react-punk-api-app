5c3ea7ce3306b673c73409540559329735869e4c
Sasha O'Donovan <sasha.odonovan@gmail.com>
2022-02-15 11:07:06 +0000

    added no jekyll

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -17,25 +17,31 @@ const SideNav = (props) => {
   } = props;
 
   return (
-    <section className={styles.sideNav__container}>
-      <h1>PUNK API</h1>
-      <SearchBar
-        userBeerSearch={userBeerSearch}
-        setUserBeerSearch={setUserBeerSearch}
-        classname={styles.sideNav__searchbar}
-      />
-      <SearchButtons
-        classname={styles.sideNav__filterbuttons}
-        abvCheck={abvCheck}
-        setAbvCheck={setAbvCheck}
-        classicCheck={classicCheck}
-        setClassicCheck={setClassicCheck}
-        acidicCheck={acidicCheck}
-        setAcidicCheck={setAcidicCheck}
-      />
+    <>
+      <section className={styles.sideNav__container}>
+        <h1>PUNK API</h1>
+        <SearchBar
+          userBeerSearch={userBeerSearch}
+          setUserBeerSearch={setUserBeerSearch}
+          classname={styles.sideNav__searchbar}
+        />
+        <SearchButtons
+          classname={styles.sideNav__filterbuttons}
+          abvCheck={abvCheck}
+          setAbvCheck={setAbvCheck}
+          classicCheck={classicCheck}
+          setClassicCheck={setClassicCheck}
+          acidicCheck={acidicCheck}
+          setAcidicCheck={setAcidicCheck}
+        />
 
-      <Button setButton="Reset Search" />
-    </section>
+        <Button setButton="Reset Search" />
+
+        <footer className={styles.sideNav__footer}>
+          Created by Sasha O'Donovan | 2022
+        </footer>
+      </section>
+    </>
   );
 };
 
